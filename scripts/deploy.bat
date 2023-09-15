06a843a239e3358f0d602fa917467b997545d6f7
Filip Celejewski <FilipCelejewski@wp.pl>
2023-09-15 11:26:21 +0200

    Funkcja lambda wysyła wiadomości w odpowiednim topicu

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,3 +1,4 @@
 call 7z a ..\build\function.zip ..\src\backend\index.js
 call aws s3 cp ..\build\function.zip s3://filcel-deployments/function.zip
-call aws cloudformation update-stack --stack-name filcel-stack-dev --template-body file://../src/backend/template.yml --parameters ParameterKey=Environment,ParameterValue=dev
+call aws cloudformation update-stack --stack-name filcel-stack-dev --template-body file://../src/backend/template.yml --parameters ParameterKey=Environment,ParameterValue=dev --capabilities CAPABILITY_NAMED_IAM
+call aws cloudformation describe-stack-events --stack-name filcel-stack-dev
