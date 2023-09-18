75129594966410ef8ca74e8fd2588fb10f809aaa
Filip Celejewski <FilipCelejewski@wp.pl>
2023-09-18 14:27:28 +0200

    Updating powershell deployment script

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,3 +1,6 @@
+Write-Host "Creating build directory"
+New-Item -ItemType Directory -Path "build"
+
 Write-Host "Compressing notification function code"
 Compress-Archive -Path ..\src\backend\index.js -DestinationPath ..\build\function.zip 
 
