649239409858a1d4c9ba9a309e0a6e0fff400532
Filip Celejewski <FilipCelejewski@wp.pl>
2023-09-18 14:51:05 +0200

    Updating powershell deployment script

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -10,5 +10,8 @@ Write-Host "Compressing notification function code"
 Compress-Archive -Path src\backend\index.js -DestinationPath $BuildDirectoryName\function.zip
 
 Write-Host "Uploading notification function zip to S3 bucket"
+aws s3 cp $BuildDirectoryName\function.zip s3://filcel-deployments/function.zip
 
 Write-Host "Creating stack using CloudFormation"
+
+aws cloudformation create-stack --stack-name filcel-stack-dev --template-body file://../src/backend/template.yml --parameters ParameterKey=Environment,ParameterValue=dev --capabilities CAPABILITY_NAMED_IAM
