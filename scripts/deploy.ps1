f167c94758565e6eb349a31873d2538589b287b4
Filip Celejewski <FilipCelejewski@wp.pl>
2023-09-18 14:25:28 +0200

    Switching from Windows shell to Powershell

---
+++ scripts/deploy.ps1
@@ -0,0 +1,6 @@
+Write-Host "Compressing notification function code"
+Compress-Archive -Path ..\src\backend\index.js -DestinationPath ..\build\function.zip 
+
+Write-Host "Uploading notification function zip to S3 bucket"
+
+Write-Host "Creating stack using CloudFormation"
