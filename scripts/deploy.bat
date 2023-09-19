542a52ada4195ef0e587a5ddc2430e83008d6bd3
Filip Celejewski <FilipCelejewski@wp.pl>
2023-09-19 09:58:32 +0200

    power shell odrazu updatuje chmure

--- scripts/deploy.bat
+++
@@ -1,4 +0,0 @@
-call 7z a ..\build\function.zip ..\src\backend\index.js
-call aws s3 cp ..\build\function.zip s3://filcel-deployments/function.zip
-call aws cloudformation create-stack --stack-name filcel-stack-dev --template-body file://../src/backend/template.yml --parameters ParameterKey=Environment,ParameterValue=dev --capabilities CAPABILITY_NAMED_IAM
-call aws cloudformation describe-stack-events --stack-name filcel-stack-dev
--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -12,6 +12,14 @@ Compress-Archive -Path src\backend\index.js -DestinationPath $BuildDirectoryName
 Write-Host "Uploading notification function zip to S3 bucket"
 aws s3 cp $BuildDirectoryName\function.zip s3://filcel-deployments/function.zip
 
-Write-Host "Creating stack using CloudFormation"
+aws cloudformation describe-stacks --stack-name filcel-stack-dev
+Write-Host $LASTEXITCODE
 
-aws cloudformation create-stack --stack-name filcel-stack-dev --template-body file://src/backend/template.yml --parameters ParameterKey=Environment,ParameterValue=dev --capabilities CAPABILITY_NAMED_IAM
+if ($LASTEXITCODE -eq 0)
+{
+    Write-Host "Updating stack using CloudFormation"
+    aws cloudformation update-stack --stack-name filcel-stack-dev --template-body file://src/backend/template.yml --parameters ParameterKey=Environment,ParameterValue=dev --capabilities CAPABILITY_NAMED_IAM
+} else {
+    Write-Host "Creating stack using CloudFormation"
+    aws cloudformation create-stack --stack-name filcel-stack-dev --template-body file://src/backend/template.yml --parameters ParameterKey=Environment,ParameterValue=dev --capabilities CAPABILITY_NAMED_IAM
+}
